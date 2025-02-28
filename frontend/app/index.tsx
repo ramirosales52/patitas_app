4b6794606ccda879975122b3e24ed702391601d6
Ramiro Rosales <ramirosales52@gmail.com>
2025-02-28 17:40:17 -0300

    Tailwind

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
@@ -9,7 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-lg font-bold">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
